38bc632c199052ea5825525cef75b27ed7c7b660
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-07-02 00:30:09 -0300

    novas

--- app/src/main/java/com/canguru/Splash.java
+++ app/src/main/java/com/canguru/Splash.java
@@ -3,17 +3,21 @@ package com.canguru;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.PixelFormat;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class Splash extends Activity {
 
     ImageView imageview_logo_splash;
+    ProgressBar progress_bar_splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +25,17 @@ public class Splash extends Activity {
         setContentView(R.layout.view_splash);
 
         imageview_logo_splash = (ImageView) findViewById(R.id.imageview_logo_splash);
-        animation_logo(imageview_logo_splash);
+        progress_bar_splash = (ProgressBar) findViewById(R.id.progress_bar_splash);
+        animation_progress_bar(progress_bar_splash);
 
     }
 
-    public void animation_logo(ImageView imageView){
-        ScaleAnimation animation = new ScaleAnimation(0,1,0,1);
-        animation.setDuration(350);
+    private void animation_progress_bar (ProgressBar progressBar){
+        TranslateAnimation animation = new TranslateAnimation(0,0,500,0);
+        animation.setDuration(1000);
         animation.setFillAfter(true);
         animation.setFillEnabled(true);
-        imageView.setAnimation(animation);
+        progressBar.setAnimation(animation);
         animation.start();
     }
 
